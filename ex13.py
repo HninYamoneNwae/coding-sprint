3bd5de06d04eb42a61cb41ac16e2dd64fa5ac690
HninYamoneNwae <hninyamonenway@gmail.com>
2018-06-25 21:14:28 +0630

    13th

---
+++ ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+script, first, second, third = [1, 2, 3, 4]
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
